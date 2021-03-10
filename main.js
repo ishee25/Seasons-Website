28c7d16daed304a5c40e46640d0c3bb2a816237c
ishee25 <74249367+ishee25@users.noreply.github.com>
2021-03-10 14:22:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function winterRedirect(){
+    window.open("winter.html");
+}
+
+function summerRedirect(){
+    window.open("summer.html");
+}
+
+function fallRedirect(){
+    window.open("fall.html");
+}
+
+function springRedirect(){
+    window.open("spring.html");
+}
